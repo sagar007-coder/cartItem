a60194f1c79c7b5c62de298d3267360550d96b15
sagar007-coder <sagarsrivastav4321@gmail.com>
2022-11-16 16:32:02 +0530

    added functionality to delete , increase and decrease button

--- src/Cart.js
+++ src/Cart.js
@@ -1,18 +1,84 @@
-import React from "react";
+import React from 'react';
 import CartItem from './CartItem';
 
-
-class Cart extends React.Component{
-    render () {
-        return (
-            <div className="cart"> 
-                <CartItem /> 
-                <CartItem />
-                <CartItem />
-            </div>
-            
-        );
+class Cart extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 99,
+          title: 'Watch',
+          qty: 1,
+          img: '',
+          id: 1
+        },
+        {
+          price: 999,
+          title: 'Mobile Phone',
+          qty: 1,
+          img: '',
+          id: 2
+        },
+        {
+          price: 999,
+          title: 'Laptop',
+          qty: 1,
+          img: '',
+          id: 3
+        }
+      ]
     }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.testing();
+  }
+  handleIncreaseQuantity = (product) =>{
+  console.log('Hey plz inc the qty of ',product);
+  const { products } = this.state;
+  const index = products.indexOf(product);
+  products[index].qty += 1;
+  this.setState({
+    products
+  })
 
+  }
+  handleDecreaseQuantity = (product) =>{
+    console.log('Hey plz dec the qty of ',product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    if(products[index].qty===0)
+    return
+    products[index].qty -= 1;
+    this.setState({
+      products
+    })
+  
+    }
+    handleDeleteProduct = (id) => {
+      const { products} = this.state;
+      const items = products.filter((item) => item.id !== id);
+      this.setState ({
+        products : items
+      })
+    }
+  render () {
+    const { products } = this.state;
+    return (
+      <div className="cart">
+        {products.map((product) => {
+          return (
+            <CartItem
+              product={product}
+              key={product.id}
+              onIncreaseQuantity ={this.handleIncreaseQuantity}
+              onDecreaseQuantity ={this.handleDecreaseQuantity}
+              onDeleteProduct ={this.handleDeleteProduct}
+            />
+          )
+        })}
+      </div>
+    );
+  }
 }
+
 export default Cart;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,72 +1,55 @@
-import React from "react";
+import React from 'react';
 
-class CartItem extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            price: 999,
-            title: 'Mobile Phone',
-            qty: 1,
-            img: ''
-        }
-
-    }
-    increaseQuantity = () =>{
-        this.setState({
-            qty : this.state.qty + 1
-        })
-    }
-    decreaseQuantity = () =>{
-        const { qty } = this.state;
-        if(qty===0)
-        return
-        this.setState({
-            qty : this.state.qty - 1
-        })
-    }
-    render(){
-        const { price , title , qty} = this.state;
-        return (
-            <div className="cart-item">
-                <div className="left-block">
-                    <img style={styles.image}/>
-
-                </div>
-                <div className="right-block">
-                    <div style={{fontSize: 25}}>{title}</div>
-                    <div style={{color: '#777'}}>Rs {price}</div>
-                    <div style={{color: '#777'}}>Qty : {qty}</div>
-                    <div className="cart-item-actions">
-                        {/* Buttons */}
-                        <img alt="increase" 
-                        className="action-icons" 
-                        src="https://cdn-icons-png.flaticon.com/512/1828/1828919.png"
-                        onClick = {this.increaseQuantity}
-                        />
-                        <img alt="decrease" 
-                        className="action-icons" 
-                        src="https://cdn-icons-png.flaticon.com/512/1828/1828899.png"
-                        onClick={this.decreaseQuantity}
-                        />
-                        <img alt="delete" 
-                        className="action-icons" 
-                        src="https://cdn-icons-png.flaticon.com/512/484/484611.png"
-                        />
-
-
-                    </div>
-
-                </div>
-            </div>
-        )
-    }
+class CartItem extends React.Component {
+ 
+  render () {
+    console.log('this.props', this.props);
+    const { price, title, qty } = this.props.product;
+    const {product , onIncreaseQuantity ,onDecreaseQuantity , onDeleteProduct} = this.props;
+    return (
+      <div className="cart-item">
+        {this.props.jsx}
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src='https://cdn-icons-png.flaticon.com/512/1828/1828919.png'
+              onClick={() => this.props.onIncreaseQuantity(this.props.product)}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src='https://cdn-icons-png.flaticon.com/512/1828/1828899.png'
+              onClick={() => this.props.onDecreaseQuantity(this.props.product)}
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src='https://cdn-icons-png.flaticon.com/512/484/484611.png'
+              onClick={() => onDeleteProduct(product.id)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
 const styles = {
-    image:{
-        height : 110,
-        width : 100,
-        borderRadius: 4,
-        background : '#ccc'
-    }
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: '#ccc'
+  }
 }
+
 export default CartItem;
