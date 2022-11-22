0eb11badd361f6a64b0741c15959a24eed07d02e
sagar007-coder <sagarsrivastav4321@gmail.com>
2022-11-23 01:09:06 +0530

    Made connection with firebase and added loader

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 //import CartItem from './CartItem';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from 'firebase/compat/app';
+import 'firebase/firestore';
+import {} from 'firebase/compat/firestore'
+import { doc } from 'firebase/firestore';
 
+// import firebase  from 'firebase/compat/app';
+// import 'firebase/compat/firestore';
+// import 'firebase/firestore';
 
 
 class App extends React.Component {
@@ -10,33 +17,40 @@ class App extends React.Component {
   constructor () {
     super();
     this.state = {
-      products: [
-        {
-          price: 99,
-          title: 'Watch',
-          qty: 1,
-          img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
-          id: 1
-        },
-        {
-          price: 999,
-          title: 'Mobile Phone',
-          qty: 1,
-          img: 'https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=367&q=80',
-          id: 2
-        },
-        {
-          price: 999,
-          title: 'Laptop',
-          qty: 1,
-          img: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=820&q=80',
-          id: 3
-        }
-      ]
+      products: [],
+      loading: true
     }
     // this.increaseQuantity = this.increaseQuantity.bind(this);
     // this.testing();
   }
+  
+  componentDidMount () {
+    firebase
+      .firestore()
+      .collection('products')
+      .get()
+      .then((snapshot)=>{
+        //console.log(snapshot);
+
+        snapshot.docs.map((doc) =>{
+          console.log(doc.data())
+
+        });
+        const products = snapshot.docs.map((doc) => {
+          const data = doc.data();
+
+          data['id'] = doc.id;
+          return doc.data();
+
+        })
+        this.setState({
+          products,
+          loading: false
+        })
+      })
+
+
+    }
   handleIncreaseQuantity = (product) =>{
     console.log('Hey plz inc the qty of ', product);
   const { products } = this.state;
@@ -96,7 +110,7 @@ class App extends React.Component {
 
     
     render () {
-      const { products } = this.state;
+      const { products , loading} = this.state;
       return (
         <div className="App">
           <Navbar count={this.getCartCount()} />
@@ -106,6 +120,7 @@ class App extends React.Component {
             onDecreaseQuantity ={this.handleDecreaseQuantity}
             onDeleteProduct ={this.handleDeleteProduct}
           />
+          {loading && <h1>Hang On!! Products are getting loaded......</h1>}
           <div style={{padding: 10 , fontSize: 20}}>TOTAL: {this.getCartTotal()}</div>
         </div>
      );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+//import * as serviceWorker from "./serviceWorker";
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore";
+ import firebase from 'firebase/compat/app';
 
 
-ReactDOM.render(<App />,document.getElementById('root'));
+const firebaseConfig = {
+  apiKey: "AIzaSyDxoHcHC4Pxpk8wbfOb0i9psJo-AJegft4",
+  authDomain: "cart-40d55.firebaseapp.com",
+  projectId: "cart-40d55",
+  storageBucket: "cart-40d55.appspot.com",
+  messagingSenderId: "387236037930",
+  appId: "1:387236037930:web:ffce870c9931b1eae54a53"
+};
+// Initialize Firebaseco
+// console.log("Firebase connected");
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+export default getFirestore;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+// firebase.initializeApp(firebaseConfig);
 
+// ReactDOM.render(<App />, document.getElementById("root"));
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
